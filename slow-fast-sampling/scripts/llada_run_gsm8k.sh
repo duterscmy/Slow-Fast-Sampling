ff9f71b985f81197df0d72e1367ef34446f81f14
mingyu cao <m.cao@surrey.ac.uk>
2026-04-06 15:47:48 +0800

    add

--- slow-fast-sampling/scripts/llada_run_gsm8k.sh
+++ slow-fast-sampling/scripts/llada_run_gsm8k.sh
@@ -1,3 +1,22 @@
+#!/bin/bash
+#SBATCH --job-name="soar_eval"
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --gres=gpu:1               # 请求2块GPU
+#SBATCH --partition=a100
+#SBATCH --time=12:00:00
+#SBATCH -o slurm.%j.%N.out
+#SBATCH -e slurm.%j.%N.err
+
+### 激活conda环境
+source ~/.bashrc # 你的环境名
+conda activate ttrl_env
+cd /mnt/fast/nobackup/scratch4weeks/mc03002/Slow-Fast-Sampling/slow-fast-sampling
+
+export HF_ENDPOINT=https://hf-mirror.com
+export HF_DATASETS_OFFLINE=0
+export CUDA_VISIBLE_DEVICES=0
+
 accelerate launch --config_file eval_config.yaml evaluation_script.py -m lm_eval --model LLADA --tasks gsm8k_cot_zeroshot --batch_size 1 \
 --model_args "pretrained=/mnt/fast/nobackup/scratch4weeks/mc03002/models/LLaDA-8B-Instruct,parallelize=False,backend="causal",mc_num=128,is_feature_cache=False,is_cfg_cache=False" \
 --gen_kwargs "block_length=32,gen_length=256,cfg_scale=0.0 "  \
